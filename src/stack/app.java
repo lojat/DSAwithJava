df6abbdd8e3dd45b18aac88fed6a22794e3f4a5e
lojat <lokeshjatavlj1998@gmail.com>
2024-07-19 17:46:35 +0530

    Stack

--- src/stack/app.java
+++ src/stack/app.java
@@ -3,21 +3,21 @@ package stack;
 //stack implementation with array
 
 public class app {
-	//stack with size 100
+	 
 	static int stack[] = new int[100];
 	//top element holding address of last in element
 	static int  top=0; 
 	
 	public static void main(String[] args) {
 		//push some elements in stack
-		push(50);//add 50 in stack
-		push(20);//add 20 in stack
-		push(0);//add 0 in stack
-		push(90);//add 90 in stack
+		push(50); 
+		push(20); 
+		push(0); 
+		push(90); 
 		
 		//pop some element from stack
-		pop();//remove 90
-		pop();//remove 0
+		pop(); 
+		pop(); 
  
 		System.out.println("peek :"+peek());//print top element 20
 		
@@ -34,19 +34,19 @@ public class app {
 	}
 	//push method
 	static int push(int ele) {
-		stack[top++]=ele;// add element in top of the array
-		return stack[top-1];//return element
+		stack[top++]=ele; 
+		return stack[top-1]; 
 	}
 	
 	//pop method
 	static int pop() {
-		int ele=stack[top-1];// last inserted element
-		stack[top--]=0;//set array top to 0
-		return ele;//return top element 
+		int ele=stack[top-1]; 
+		stack[top--]=0; 
+		return ele; 
 	}
 	
 	//peek method
 	static int peek() {
-		return stack[top-1];//return top element
+		return stack[top-1]; 
 	}
 }
